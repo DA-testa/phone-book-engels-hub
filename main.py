f7e97a42d3e073d87933695fcfdd13cf1661885b
Artjoms Papšs <68332986+engels-hub@users.noreply.github.com>
2023-04-02 21:22:46 +0000

    fix del#2

--- main.py
+++ main.py
@@ -88,14 +88,14 @@ def find(query, buckets):
 def delete(query, buckets):
     hashed= make_hash(query)
     bucket = buckets[hashed]
-    index=False
+    i=False
     for item in bucket:
         if item[0]==query:
-            index=True
+            i=True
             bucket.remove(item)
             buckets[hashed]=bucket
             
-    if not index:
+    if not i:
         print("not found")
     return buckets
 
